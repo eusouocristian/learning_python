4a47d8ca0b53d408bea3d32150527ac6ccf340e2
eusouocristian <eusouocristian@hotmail.com>
2020-02-09 16:01:55 -0300

    atualização

--- 1-inverted_string.py
+++
@@ -1,6 +0,0 @@
-import os
-clear = lambda: os.system('cls')
-clear()
-data = input('type a string:\n>> ')
-data_inverted = data[::-1]
-print('The inverted string is\n>>',data_inverted)
--- run_python.m
+++
@@ -1,5 +0,0 @@
-commandStr = 'python C:/Users/Cristian/Dropbox/Python/hello.py';
-[status, commandOut] = system(commandStr);
-if status==0
-    fprintf('The result is %sa\n',commandOut);
-end
