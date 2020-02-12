8393cd89f56836ef2ff9fb23c9bd4003ac63849a
eusouocristian <eusouocristian@hotmail.com>
2020-02-12 14:45:12 -0300

    ok

---
+++ Learning Python/4-Class4.py
@@ -0,0 +1,18 @@
+# Metodos especiais 
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    # permite que o resultado seja impresso na forma de texto, (print(A))
+    def __str__(self):
+        return 'Vector({},{})'.format(self.x,self.y)
+
+    def __add__(self, other):
+        soma = Vector(self.x + other.x, self.y + other.y)
+        return soma
+        
+A = Vector(1,3)
+B = Vector(4,-1)
+print(A+B)
