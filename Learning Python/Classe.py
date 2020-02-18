b80e9f6a2abd9f12707ddf9af9130719da91f69b
eusouocristian <eusouocristian@hotmail.com>
2020-02-18 14:19:00 -0300

    ok

--- Learning Python/Classe.py
+++ Learning Python/Classe.py
@@ -65,6 +65,25 @@ class Liar(list):
 # a = Classe.Liar([1,2,3])
 # len(a)              (devera retornar 13)
 
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return '{} by {}'.format(self.title, self.author)
+    
+class Bookcase:
+    def __init__(self, books=None):   #self, and a list of books, none as default value
+        self.books = books
+
+    @classmethod
+    def create_bookcase(cls, book_list):
+        books = []
+        for title, author in book_list:
+            books.append(Book(title, author))
+        return cls(books)
+
 
 
 
