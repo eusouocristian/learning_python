3e50462e88b2f228ca38ba00d338ed8bb72006a9
eusouocristian <eusouocristian@hotmail.com>
2020-05-26 18:59:53 -0300

    ok

---
+++ Django_treinaweb/gerenciador_tarefas/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Cristian/GoogleDrive/Dropbox/Code/learning_python/Django_treinaweb/gerenciador_tarefas/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
