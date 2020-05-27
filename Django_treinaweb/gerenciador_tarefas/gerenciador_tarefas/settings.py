65bc164faf9ffd9ea1077a55db17bfe2c489b9b6
eusouocristian <eusouocristian@hotmail.com>
2020-05-27 14:19:30 -0300

    ok

--- Django_treinaweb/gerenciador_tarefas/gerenciador_tarefas/settings.py
+++ Django_treinaweb/gerenciador_tarefas/gerenciador_tarefas/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import heroku as heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +127,7 @@ LOGIN_URL = 'logar_usuario'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+heroku.settings(locals())
+
