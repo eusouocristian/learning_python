b150ad6dee566c56c2f35dd099a1b3ea27704475
Cristian Figueiredo dos Santos <eusouocristian@hotmail.com>
2022-11-24 10:40:27 -0500

    Delete Whatsapp directory

--- Whatsapp /send_sms.py
+++
@@ -1,6 +0,0 @@
-import messagebird
-client = messagebird.Client('l6AqewjeVnuy1NcfGET76lLIk')
-message = client.message_create('MessageBird',
-                                '+5551998034545',
-                                'Eu te amo muitão. (mandando via Python)',
-                                {'reference': 'Foobar'})
--- Whatsapp /token_mobilio.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def generate_token():
-    os.environ['account_sid'] = 'ACf24d7582a399c365360e9ebafb78fe74'
-    os.environ['auth_token'] = 'c116d1f06137c56dedf9b42e6333e311'
